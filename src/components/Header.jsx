12811f1a7248624e8e5d2a750550280111fcb842
Tavishaa <tavishaajaiswall@gmail.com>
2025-01-04 23:24:11 +0530

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,7 +26,7 @@ function Header() {
             animate={{ opacity: 1, x: 0 }}
             className="flex items-center"
           >
-            <img src="/logo.png" alt="Company Logo" className="h-12" />
+            <img src="https://plus.unsplash.com/premium_photo-1661914978519-52a11fe159a7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bG9nb3xlbnwwfHwwfHx8MA%3D%3D" alt="Company Logo" className="h-12" />
           </motion.div>
 
           <nav className="hidden md:flex space-x-6 font-primary">
